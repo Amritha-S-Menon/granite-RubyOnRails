cf95b24f005f6f3001ec532471c655a96cac0bf4
Amritha Menon <amrithamenon@Amrithas-MacBook-Pro.local>
2021-10-12 17:32:48 +0530

    Added show page for a task

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -6,4 +6,12 @@ class TasksControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  def show
+    task = Task.find_by(slug: params[:slug])
+    if task
+      render status: :ok, json: { task: task }
+    else
+      render status: :not_found, json: { error: "Task not found" }
+    end
+  end
 end
